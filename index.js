c9375e0478eb6137ba594a30427aef34e2d3839f
Gael Hofman <Gieltie@outlook.fr>
2023-06-16 16:45:58 +0200

    used the event prevent default and addes 2 buttons

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const modalBox = document.getElementById('modal')
-const modalBoxBtn = document.getElementById('modal-close-btn')
+const modal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
 
-setTimeout(() => {
-    modalBox.style.display = 'inline'
+setTimeout(function(){
+    modal.style.display = 'inline'
 }, 1500)
 
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
+})
 
-modalBoxBtn.addEventListener("click", () => {
-    modalBox.style.display = 'none'
+consentForm.addEventListener('submit', function(e){
+    e.preventDefault()
 })
